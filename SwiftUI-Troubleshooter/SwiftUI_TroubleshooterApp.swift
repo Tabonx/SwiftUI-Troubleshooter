abe7a4e3ded3917732c4ca08b5822fa4ffbb7291
Pavel Kroupa <kroupa.pavel@icloud.com>
2024-11-27 21:35:49 +0100

    Initial Commit

---
+++ SwiftUI-Troubleshooter/SwiftUI_TroubleshooterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TroubleshooterApp.swift
+//  SwiftUI-Troubleshooter
+//
+//  Created by Pavel Kroupa on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TroubleshooterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
